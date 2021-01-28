0d347c4e0caaeba90a9fcf36656f15dd90f83409
Bartosz Horoba <bartosz.horoba@gmail.com>
2021-01-28 21:04:47 +0100

    Fix links in app

--- src/components/features/BurgerMenu/BurgerMenu.js
+++ src/components/features/BurgerMenu/BurgerMenu.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './BurgerMenu.module.scss';
-import { Link, BrowserRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 
@@ -22,32 +22,26 @@ const BurgerMenu = ({ categories }) => {
         >
           <ul className={styles.navMenuItems}>
             <li className={styles.navToggle}>
-              <BrowserRouter basename={process.env.PUBLIC_URL}>
-                <Link to='/'>
-                  <AiIcons.AiOutlineClose
-                    onClick={showSidebar}
-                    className={styles.iconClose}
-                  />
-                </Link>
-              </BrowserRouter>
+              <Link to={`${process.env.PUBLIC_URL}/`}>
+                <AiIcons.AiOutlineClose
+                  onClick={showSidebar}
+                  className={styles.iconClose}
+                />
+              </Link>
             </li>
             <li className={styles.item}>
-              <BrowserRouter basename={process.env.PUBLIC_URL}>
-                <Link to='/'>
-                  <FaIcons.FaHome />
-                  <span>Home</span>
-                </Link>
-              </BrowserRouter>
+              <Link to={`${process.env.PUBLIC_URL}/`}>
+                <FaIcons.FaHome />
+                <span>Home</span>
+              </Link>
             </li>
             {categories.map((item, index) => {
               return (
                 <li key={index} className={styles.item}>
-                  <BrowserRouter basename={process.env.PUBLIC_URL}>
-                    <Link to={`/shop/${item.id}`}>
-                      {item.icon}
-                      <span>{item.name}</span>
-                    </Link>
-                  </BrowserRouter>
+                  <Link to={`${process.env.PUBLIC_URL}/shop/${item.id}`}>
+                    {item.icon}
+                    <span>{item.name}</span>
+                  </Link>
                 </li>
               );
             })}
